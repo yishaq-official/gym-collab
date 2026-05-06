41dc054f75ed93aa73cb555ae5dce389e1edd74e
ahmedseid9392 <dbu1601509@dbu.edu.et>
2026-05-06 14:29:19 +0300

    DGMS-31 FS-02: i ipliment Schedule view in footer section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useEffect, useMemo, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import ProtectedRoute from './auth/ProtectedRoute'
@@ -99,16 +98,3 @@ export default function App() {
     </>
   )
 }
-=======
-
-function App() {
-
-  return (
-    <>
-    Hellow
-    </>
-  )
-}
-
-export default App
->>>>>>> 8651c482823006903a48906e45ab4cd3aa813243
